b87c64f239d2669e28fd023e3d57e6d71e151d09
janardan1234 <92309600+janardan1234@users.noreply.github.com>
2023-05-07 21:49:30 +0545

    Create random_password_generator.py

---
+++ random_password_generator.py
@@ -0,0 +1,17 @@
+import random
+
+import string as str
+
+letter = str.ascii_letters
+number = str.digits
+punctuation = str.punctuation
+
+
+def random_password_generator(len: int):
+    cahracters = letter+number+punctuation
+    random_password = ''.join(random.choice(cahracters) for i in range(len))
+    print(random_password)
+
+
+pass_length = int(input("Enter password length: "))
+random_password_generator(pass_length)
