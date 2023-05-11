700fb392891bec64bdea50412ff297c84f9fa888
janardan1234 <92309600+janardan1234@users.noreply.github.com>
2023-05-11 18:01:30 +0545

    Random_password_generator.py

--- random_password_generator.py
+++ random_password_generator.py
@@ -9,10 +9,14 @@ punctuation = str.punctuation
 
 def random_password_generator(len: int):
     cahracters = letter+number+punctuation
-    random_password = ''.join(random.choice(cahracters) for i in range(len))
-    print(random_password)
+    try:
+        random_password = ''.join(random.choice(cahracters)for i in range(len))
+        return random_password
+    except ValueError:
+        print(f"Enter numbers only {len} isn't a number.")
 
 
 if __name__ == "__main__":
     pass_length = int(input("Enter password length: "))
-    random_password_generator(pass_length)
+    password = random_password_generator(pass_length)
+    print(password)
