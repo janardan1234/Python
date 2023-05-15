7248520c68d1a311307f565a0750e94bb9189cb1
janardan1234 <92309600+janardan1234@users.noreply.github.com>
2023-05-15 08:38:09 +0545

    Update random_password_generator.py

--- random_password_generator.py
+++ random_password_generator.py
@@ -1,22 +1,26 @@
 import random
 
-import string as str
-
-letter = str.ascii_letters
-number = str.digits
-punctuation = str.punctuation
+import string
 
 
-def random_password_generator(len: int):
-    cahracters = letter+number+punctuation
+def random_password_generator(length: int):
+    char = string.printable.replace('\n', '')
     try:
-        random_password = ''.join(random.choice(cahracters)for i in range(len))
+        random_password = ''.join(random.choice(char)for i in range(length))
         return random_password
     except ValueError:
-        print(f"Enter numbers only {len} isn't a number.")
+        print(f"Enter numbers only {length} isn't a number.")
+
+
+def generate_password(length: int):
+
+    if length <= 8 or length > 16:
+        return "Password must be between 8 and 16 letters."
+    else:
+        return random_password_generator(length)
 
 
 if __name__ == "__main__":
     pass_length = int(input("Enter password length: "))
-    password = random_password_generator(pass_length)
+    password = generate_password(pass_length)
     print(password)
